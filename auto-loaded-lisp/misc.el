a88a8c5f97fcb65c11437cc2832593f6df408016
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2014-12-29 18:17:07 +0100

    Increase the ridiculously small filename field in ibuffer

--- auto-loaded-lisp/misc.el
+++ auto-loaded-lisp/misc.el
@@ -100,4 +100,16 @@
   ;; use spotlight for locate
   (setq locate-command "mdfind"))
 
+(require 'ibuffer)
+(setq ibuffer-formats '((mark modified read-only " "
+                              (name 36 36 :left :elide)
+                              " "
+                              (size 9 -1 :right)
+                              " "
+                              (mode 16 16 :left :elide)
+                              " " filename-and-process)
+                        (mark " "
+                              (name 16 -1)
+                              " " filename)))
+
 (provide 'misc)
