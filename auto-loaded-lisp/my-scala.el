0c122c3521a9448dd984ed8cd0905c648ab4c533
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-02-24 11:50:57 +0100

    Rename scala-mode

--- auto-loaded-lisp/my-scala.el
+++ auto-loaded-lisp/my-scala.el
@@ -1,2 +1,7 @@
+(require 'scala-mode)
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
 (add-to-list 'auto-mode-alist '("\\.sbt$" . scala-mode))
+
+(define-key scala-mode-map [C-tab] 'other-window)
+
+(provide 'my-scala)
