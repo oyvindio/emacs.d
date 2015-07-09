8d6c13dac86b3566d23ca340c78d967b3bc796e2
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-07-09 09:37:14 +0200

    Readline

--- lisp/my-helm.el
+++ lisp/my-helm.el
@@ -43,6 +43,7 @@
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+(define-key helm-map (kbd "C-w") 'backward-kill-word) ; readline muscle memory is too damn high
 
 
 
