fce78da9cf3ac4fbaee216d77d6aa6e9e64c675b
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-09-21 13:43:06 +0200

    Add my-nxml.el

---
+++ auto-loaded-lisp/my-nxml.el
@@ -0,0 +1,13 @@
+(require 'nxml-mode)
+
+(defun nxml-toggle-indent ()
+  (interactive)
+  (if (eq nxml-child-indent 2)
+      (progn
+        (setq nxml-child-indent 4)
+        (display-message-or-buffer "Indenting with 4 spaces")) 
+    (progn
+      (setq nxml-child-indent 2)
+      (display-message-or-buffer "Indenting with 2 spaces"))))
+
+(provide 'my-nxml)
