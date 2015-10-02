9c55cf348fddd2704a6f890df99c7c2aa7f19d4d
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-10-02 09:21:01 +0200

    run-coding-hook in puppet-mode

--- auto-loaded-lisp/puppet.el
+++ auto-loaded-lisp/puppet.el
@@ -1,4 +1,6 @@
 (autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
 
+(add-hook 'puppet-mode-hook 'run-coding-hook)
+
 (provide 'puppet)
