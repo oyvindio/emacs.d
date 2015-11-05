6b4c0401d3ae27260565900ec9974f696d0817d2
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-11-05 13:19:04 +0100

    Syntax highlight org-babel blocks

--- auto-loaded-lisp/my-org.el
+++ auto-loaded-lisp/my-org.el
@@ -1,9 +1,12 @@
 (require 'org)
 
+(setq org-src-fontify-natively t)
+
 (defun my-org-autodone (n-done n-not-done)
   "Switch entry to DONE when all subentries are done, to TODO otherwise."
   (let (org-log-done org-log-states)   ; turn off logging
     (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
 
 (add-hook 'org-after-todo-statistics-hook 'my-org-autodone)
+
 (provide 'my-org)
