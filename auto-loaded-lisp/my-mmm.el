17c16482d1ba655cd87fafc3bfb019dfc2371d40
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-12-22 15:11:37 +0100

    Fixes for mmm-mode

--- auto-loaded-lisp/my-mmm.el
+++ auto-loaded-lisp/my-mmm.el
@@ -2,6 +2,21 @@
 (setq mmm-global-mode 'maybe)
 
 (mmm-autoload-class 'erb "mmm-erb")
-(mmm-add-mode-ext-class nil "\\.erb\\'" 'erb)
+(add-to-list 'auto-mode-alist '("\\.y\\(a\\)?ml\\.erb$" . yaml-mode))
+(mmm-add-mode-ext-class 'yaml-mode "\\.y\\(a\\)?ml\\.erb\\'" 'erb)
+
+(add-to-list 'auto-mode-alist '("\\.xml\\.erb$" . nxml-mode))
+(mmm-add-mode-ext-class 'nxml-mode "\\.xml\\.erb\\'" 'erb)
+
+(add-to-list 'auto-mode-alist '("\\.service\\.erb$" . conf-mode))
+(mmm-add-mode-ext-class 'conf-mode "\\.service\\.erb\\'" 'erb)
+
+(add-to-list 'auto-mode-alist '("\\.sh\\.erb$" . shell-script-mode))
+(mmm-add-mode-ext-class 'shell-script-mode "\\.sh\\.erb\\'" 'erb)
+
+(defun add-mmm-parse-buffer-on-save-hook ()
+  (add-hook 'after-save-hook 'mmm-parse-buffer nil t))
+
+(add-hook 'mmm-mode-hook 'add-mmm-parse-buffer-on-save-hook)
 
 (provide 'my-mmm)
