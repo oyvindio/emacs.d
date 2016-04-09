72620cfd94b23a1d6809c0cf4f6081d4d4f7ad79
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2016-04-09 12:53:38 +0200

    gofmt on save, go-eldoc & godef-jump

--- auto-loaded-lisp/my-go.el
+++ auto-loaded-lisp/my-go.el
@@ -2,5 +2,17 @@
 
 (exec-path-from-shell-copy-env "GOPATH")
 
+; go get github.com/nsf/gocode
 (require 'go-eldoc)
+
+(defun enable-gofmt-on-save ()
+  (add-hook 'before-save-hook 'gofmt-before-save))
+
+; go get github.com/rogpeppe/godef
+(defun enable-godef-jump ()
+  (local-set-key (kbd "M-.") 'godef-jump))
+
 (add-hook 'go-mode-hook 'go-eldoc-setup)
+(add-hook 'go-mode-hook 'enable-godef-jump)
+(add-hook 'go-mode-hook 'enable-gofmt-on-save)
+(add-hook 'go-mode-hook 'run-coding-hook)
