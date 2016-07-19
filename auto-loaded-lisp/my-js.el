69e3a3d387403c7e8b5c51249dbf9a2d25c2d544
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2016-07-19 14:11:13 +0200

    Remove broken js2 stuff

--- auto-loaded-lisp/my-js.el
+++ auto-loaded-lisp/my-js.el
@@ -4,27 +4,8 @@
 (add-hook 'js2-mode-hook 'run-coding-hook)
 (setq js2-basic-offset 2)
 (setq js-indent-level 2)
-(defadvice js2-reparse (before json)
-	(setq js2-buffer-file-name buffer-file-name))
-(ad-activate 'js2-reparse)
-
-(defadvice js2-parse-statement (around json)
-	(if (and (= tt js2-LC)
-			js2-buffer-file-name
-			(string-equal (substring js2-buffer-file-name -5) ".json")
-			(eq (+ (save-excursion
-						(goto-char (point-min))
-						(back-to-indentation)
-						(while (eolp)
-							(next-line)
-							(back-to-indentation))
-						(point)) 1) js2-ts-cursor))
-		(setq ad-return-value (js2-parse-assign-expr))
-		ad-do-it))
-(ad-activate 'js2-parse-statement)
 
 (require 'js2-refactor)
 (js2r-add-keybindings-with-prefix "C-c C-m")
 
 (provide 'my-js)
-;;; starter-kit-js.el ends here
