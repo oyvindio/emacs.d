1231f0220c207ebf22b78ac37647e9af4efec874
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2016-07-19 14:11:55 +0200

    Fuzzy-match all the things

--- lisp/my-helm.el
+++ lisp/my-helm.el
@@ -58,11 +58,7 @@
       helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount 8 ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t
-      helm-M-x-fuzzy-match t ; fuzzy-match in M-x minibuffer
-      helm-buffers-fuzzy-matching t ; fuzzy-match buffers
-      helm-recentf-fuzzy-match t ; fuzzy-match recentf
-      helm-imenu-fuzzy-match t ; fuzzy-match imenu
-      helm-apropos-function-list t ; fuzzy-match apropos
+      helm-mode-fuzzy-match t
       )
 
 (when (eq system-type 'darwin)
