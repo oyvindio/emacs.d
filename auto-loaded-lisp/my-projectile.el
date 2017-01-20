0bb54741f7276ead29e0004a0c52a62ec631c641
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2017-01-20 16:37:00 +0100

    Disable slow projectile-mode-line

--- auto-loaded-lisp/my-projectile.el
+++ auto-loaded-lisp/my-projectile.el
@@ -1,2 +1,4 @@
 (require 'projectile)
 (projectile-global-mode t)
+; default projectile-mode-line is very slow and is executed on {next,previous}-line which makes navigation very slow
+(setq projectile-mode-line " Projectile")
