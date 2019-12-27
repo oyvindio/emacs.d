a7db663e9cd94f941a58b61ca42e7e93c8a33958
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2019-12-27 21:51:46 +0100

    Add useful rust-mode keybinds

--- auto-loaded-lisp/my-rust.el
+++ auto-loaded-lisp/my-rust.el
@@ -1,6 +1,19 @@
 (autoload 'rust-mode "rust-mode" nil t)
+
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
 (setq rust-format-on-save t)
+
+(eval-after-load 'rust-mode
+  '(progn
+     (define-key rust-mode-map (kbd "C-c C-c") 'rust-compile)
+     (define-key rust-mode-map (kbd "C-c C-r") 'rust-run)
+     (define-key rust-mode-map (kbd "C-c C-t") 'rust-test)
+     ))
+
+
 (eval-after-load 'cargo
   '(progn
      (add-hook 'rust-mode-hook 'cargo-minor-mode)))
+
+(provide 'my-rust)
