38e6b2d7b20896aa6e121eec14acb74bcb732e68
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2020-03-19 10:53:35 +0100

    Disable lsp-ui

--- auto-loaded-lisp/my-lsp.el
+++ auto-loaded-lisp/my-lsp.el
@@ -1,7 +1,8 @@
 (require 'lsp-mode)
-(require 'lsp-ui)
 
-(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+; is lsp-ui-mode causing the sluggish cursor movement ?
+; (require 'lsp-ui)
+; (add-hook 'lsp-mode-hook 'lsp-ui-mode)
 
 (global-set-key (kbd "s-<f7>") 'lsp-find-references)
 (global-set-key (kbd "M-g d") 'lsp-find-definition)
