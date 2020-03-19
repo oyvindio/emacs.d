d9422e35af61ff14efbb27902b59ef7d4eec0e6e
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2020-03-19 10:53:21 +0100

    Use lsp with go-mode

--- auto-loaded-lisp/my-go.el
+++ auto-loaded-lisp/my-go.el
@@ -8,8 +8,14 @@
   (add-hook 'before-save-hook 'gofmt-before-save))
 
 
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+;; (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
 (add-hook 'go-mode-hook 'enable-gofmt-on-save)
 (add-hook 'go-mode-hook 'run-coding-hook)
+(add-hook 'go-mode-hook 'lsp)
 
 (let ((gopath (getenv "GOPATH"))
       (gopath-fallback "~/src/go")
