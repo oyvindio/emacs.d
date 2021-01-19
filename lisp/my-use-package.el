146f92ad52119b39cb5e392a6bf2ed97f792307b
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2021-01-19 21:04:13 +0100

    Force load bind-key

--- lisp/my-use-package.el
+++ lisp/my-use-package.el
@@ -4,4 +4,7 @@
     (package-install 'use-package))
   (require 'use-package))
 
+;; force load bind-key to avoid error void variable personal-keybindings in later use-package macros
+(use-package bind-key)
+
 (provide 'my-use-package)
