c26cb61c282198fd9fd64f5892dc19639b4b2a4b
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-02-14 17:11:20 +0100

    Try to use lsp for formatting and import sorting (goimports)

--- auto-loaded-lisp/my-go.el
+++ auto-loaded-lisp/my-go.el
@@ -3,21 +3,21 @@
 (exec-path-from-shell-copy-env "GOPATH")
 
 ; go get golang.org/x/tools/cmd/goimports
-(setq gofmt-command "goimports")
-(defun enable-gofmt-on-save ()
-  (add-hook 'before-save-hook 'gofmt-before-save))
+;; (setq gofmt-command "goimports")
+;; (defun enable-gofmt-on-save ()
+;;   (add-hook 'before-save-hook 'gofmt-before-save))
+;; (add-hook 'go-mode-hook 'enable-gofmt-on-save)
 
-
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-;; (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+(use-package lsp
+  :config
+  (defun lsp-go-install-save-hooks ()
+    (add-hook 'before-save-hook #'lsp-format-buffer t t)
+    (add-hook 'before-save-hook #'lsp-organize-imports t t))
+  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks))
 
 (eval-after-load 'go-mode
   '(define-key go-mode-map (kbd "<C-return>") 'go-run))
 
-
-(add-hook 'go-mode-hook 'enable-gofmt-on-save)
 (add-hook 'go-mode-hook 'run-coding-hook)
 (add-hook 'go-mode-hook 'lsp)
 
