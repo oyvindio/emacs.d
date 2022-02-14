3fa5e094d8ffd3fd01586264d0af7884d68b2f95
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-02-14 16:46:58 +0100

    Set up lsp with use-package

--- auto-loaded-lisp/my-lsp.el
+++ auto-loaded-lisp/my-lsp.el
@@ -1,10 +1,12 @@
-(require 'lsp-mode)
-
-; is lsp-ui-mode causing the sluggish cursor movement ?
-; (require 'lsp-ui)
-; (add-hook 'lsp-mode-hook 'lsp-ui-mode)
-
-(global-set-key (kbd "s-<f7>") 'lsp-find-references)
-(global-set-key (kbd "M-g d") 'lsp-find-definition)
+(use-package lsp-mode
+  :ensure t
+  :bind (:map lsp-mode-map
+              ("s-<f7>" . lsp-find-references)
+              ("M-g d" . lsp-find-definition)))
+(use-package lsp-ui
+  :ensure t
+  :bind (:map lsp-ui-mode-map
+              ("s-l" . nil)
+              ("s-l i" . lsp-ui-imenu)))
 
 (provide 'my-lsp)
