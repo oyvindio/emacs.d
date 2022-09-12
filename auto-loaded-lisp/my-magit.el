c3b2bb747f6851c0dbcea2297a58446a9a843bd0
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-09-12 16:15:26 +0200

    Autoload magit with use-package
    Remove a lot of unused / non-functioning / outdated configuration

--- auto-loaded-lisp/my-magit.el
+++ auto-loaded-lisp/my-magit.el
@@ -1,27 +1,4 @@
-;; (add-to-list 'load-path "~/Projects/git-modes")
-;; (add-to-list 'load-path "~/Projects/magit")
-;; (eval-after-load 'info
-;;   '(progn (info-initialize)
-;;           (add-to-list 'Info-directory-list  "~/Projects/magit")))
 (require 'magit)
-
-(add-hook 'magit-log-edit-mode-hook 'local-column-number-mode)
-
-(defun magit-toggle-whitespace ()
-  (interactive)
-  (if (member "-w" magit-diff-options)
-      (magit-dont-ignore-whitespace)
-    (magit-ignore-whitespace)))
-
-(defun magit-ignore-whitespace ()
-  (interactive)
-  (add-to-list 'magit-diff-options "-w")
-  (magit-refresh))
-
-(defun magit-dont-ignore-whitespace ()
-  (interactive)
-  (setq magit-diff-options (remove "-w" magit-diff-options))
-  (magit-refresh))
 
 (defadvice magit-status (around magit-fullscreen activate)
   (window-configuration-to-register :magit-fullscreen)
@@ -34,23 +11,25 @@
   (kill-buffer)
   (jump-to-register :magit-fullscreen))
 
-(eval-after-load 'magit
-  '(progn
-     (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
-     (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
-     (setq magit-backup-mode nil)
-     (setq magit-diff-refine-hunk 'all)
-     (setq git-commit-summary-max-length 50)
-     (setq git-commit-fill-column 72)
-     ))
+(use-package magit
+  :ensure t
+  :config
+  (setq magit-diff-refine-hunk 'all)
+  :commands magit-status
+  :bind
+  ("C-x g" . magit-status)
+  (:map magit-mode-map
+        ("C-<tab>" . other-window))
+  (:map magit-status-mode-map
+        ("q" . magit-quit-session)))
 
-(global-set-key (kbd "C-x g") 'magit-status)
-(define-key magit-mode-map [C-tab] 'other-window)
-(define-key magit-mode-map (kbd "C-c t") 'magit-section-toggle)
+(defun set-git-commit-fill-column ()
+  (setq fill-column 72))
 
-(when (file-exists-p "/usr/local/bin/emacsclient")
-  '(progn
-     (setq magit-emacsclient-executable "/usr/local/bin/emacsclient")
-     (setq with-editor-emacsclient-executable "/usr/local/bin/emacsclient")))
+(use-package git-commit
+  :ensure t
+  :config
+  (setq git-commit-summary-max-length 50)
+  (add-hook 'git-commit-mode-hook 'set-git-commit-fill-column))
 
 (provide 'my-magit)
