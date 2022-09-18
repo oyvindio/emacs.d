08dc05576450cc70a152e862764234c6668bd50d
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-09-18 14:38:52 +0200

    Load go-mode with use-package

--- auto-loaded-lisp/my-go.el
+++ auto-loaded-lisp/my-go.el
@@ -1,31 +1,26 @@
-(require 'go-mode)
-
-(exec-path-from-shell-copy-env "GOPATH")
-
-; go get golang.org/x/tools/cmd/goimports
-;; (setq gofmt-command "goimports")
-;; (defun enable-gofmt-on-save ()
-;;   (add-hook 'before-save-hook 'gofmt-before-save))
-;; (add-hook 'go-mode-hook 'enable-gofmt-on-save)
-
-(use-package lsp
+(use-package go-mode
+  :ensure t
+  :after lsp
   :config
+  (exec-path-from-shell-copy-env "GOPATH")
+
   (defun lsp-go-install-save-hooks ()
     (add-hook 'before-save-hook #'lsp-format-buffer t t)
     (add-hook 'before-save-hook #'lsp-organize-imports t t))
-  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks))
+  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+  (add-hook 'go-mode-hook 'run-coding-hook)
+  (add-hook 'go-mode-hook 'lsp)
 
-(eval-after-load 'go-mode
-  '(define-key go-mode-map (kbd "<C-return>") 'go-run))
-
-(add-hook 'go-mode-hook 'run-coding-hook)
-(add-hook 'go-mode-hook 'lsp)
-
-(let ((gopath (getenv "GOPATH"))
+  (let ((gopath (getenv "GOPATH"))
       (gopath-fallback "~/src/go")
       (relative-playground-basedir "playground"))
   (if (null gopath)
       (setq go-playground-basedir (concat gopath-fallback "/" relative-playground-basedir)))
   (setq go-playground-basedir (concat gopath "/" relative-playground-basedir)))
 
-(setq go-playground-go-command "GO111MODULE=off go")
+  (setq go-playground-go-command "GO111MODULE=off go")
+  :bind
+  (:map go-mode-map
+        ("C-<return>" . go-test-current-test)
+        ("C-M-<return>" . go-test-current-file))
+  :mode ("\\.go$" . go-mode))
