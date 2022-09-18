177fb68db60ff0f5f2b2aa67e080fcf996d16672
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-09-18 14:55:59 +0200

    Load json-mode with use-package

--- auto-loaded-lisp/my-json.el
+++ auto-loaded-lisp/my-json.el
@@ -1,5 +1,7 @@
-(require 'json-mode)
-(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
-(setq json-reformat:indent-width 2)
+(use-package json-mode
+  :ensure t
+  :mode ("\\.json$" . json-mode)
+  :config
+  (setq json-reformat:indent-width 2))
 
 (provide 'my-json)
