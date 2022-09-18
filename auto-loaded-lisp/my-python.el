ecd8298fa6841aef2d27b3a1381b1073c726c786
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-09-18 14:48:18 +0200

    Load python-mode with use-package

--- auto-loaded-lisp/my-python.el
+++ auto-loaded-lisp/my-python.el
@@ -1,21 +1,22 @@
-;; Load python-mode for .py and .wsgi files
-(autoload 'python-mode "python-mode" "Python editing mode." t)
-(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
-(add-to-list 'auto-mode-alist '("\\.wsgi$" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(use-package python-mode
+  :ensure t
+  :after lsp
+  :mode (("\\.py$" . python-mode)
+         ("\\.wsgi$" . python-mode))
+  :config
+  (add-hook 'python-mode-hook 'run-coding-hook)
+  (add-hook 'python-mode-hook 'lsp))
 
-(add-hook 'python-mode-hook 'run-coding-hook)
+(use-package pytest
+  :ensure t
+  :after python-mode)
 
-(require 'pytest)
-(require 'pyenv-mode)
-(eval-after-load 'python-mode
-  '(progn
-     (define-key python-mode-map (kbd "M-?") 'hippie-expand)
-     (define-key python-mode-map (kbd "C-c t") 'pytest-all)))
+(use-package pyenv-mode
+  :ensure t
+  :after python-mode)
 
-(require 'flycheck-prospector)
-
-;; (require 'lsp-mode)
-;; (add-hook 'python-mode-hook 'lsp)
+(use-package flycheck-prospector
+  :ensure t
+  :after python-mode)
 
 (provide 'my-python)
