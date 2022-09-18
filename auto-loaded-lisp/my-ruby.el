08d17fb111e1024150b07a843bb468b198ce8d4a
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-09-18 15:07:06 +0200

    Autoload ruby-mode with use-package

--- auto-loaded-lisp/my-ruby.el
+++ auto-loaded-lisp/my-ruby.el
@@ -1,39 +1,26 @@
-(eval-after-load 'ruby-mode
-  '(progn
-     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
-     (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
-     (auto-fill-mode 0)))
+(use-package ruby-mode
+  :mode (("\\.rake$" . ruby-mode)
+         ("\\.gemspec$" . ruby-mode)
+         ("\\.ru$" . ruby-mode)
+         ("Rakefile$" . ruby-mode)
+         ("Gemfile$" . ruby-mode)
+         ("Capfile$" . ruby-mode)
+         ("Vagrantfile$" . ruby-mode)
+         ("Buildfile$" . ruby-mode)
+         ("\\.spec$" . ruby-mode))
+  :config
+  (add-hook 'ruby-mode-hook 'run-coding-hook)
+  (auto-fill-mode 0)
+  (add-to-list 'completion-ignored-extensions ".rbc")
+  :bind (:map ruby-mode-map
+              ("<Return>" . reindent-then-newline-and-indent)
+              ("C-M-h" . backward-kill-word)))
 
-(global-set-key (kbd "C-h r") 'ri)
-
-;; Rake files are ruby, too, as are gemspecs, rackup files, etc.
-(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Buildfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.spec$" . ruby-mode))
-
-;; We never want to edit Rubinius bytecode
-(add-to-list 'completion-ignored-extensions ".rbc")
-
-;; Clear the compilation buffer between test runs.
-(eval-after-load 'ruby-compilation
-  '(progn
-     (defadvice ruby-do-run-w/compilation (before kill-buffer (name cmdlist))
-       (let ((comp-buffer-name (format "*%s*" name)))
-         (when (get-buffer comp-buffer-name)
-           (with-current-buffer comp-buffer-name
-             (delete-region (point-min) (point-max))))))
-     (ad-activate 'ruby-do-run-w/compilation)))
-
-(add-hook 'ruby-mode-hook 'run-coding-hook)
-
-(require 'rbenv)
-(setq rbenv-executable (executable-find "rbenv"))
-(rbenv-use-global)
+(use-package rbenv
+  :ensure t
+  :after ruby-mode
+  :config
+  (setq rbenv-executable (executable-find "rbenv"))
+  (rbenv-use-global))
 
 (provide 'my-ruby)
