d98e182d8e5451d40394609e95af93c41f78a1ab
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-10-05 10:13:51 +0200

    Apply lsp performance tuning as suggested in docs

--- auto-loaded-lisp/my-lsp.el
+++ auto-loaded-lisp/my-lsp.el
@@ -3,7 +3,14 @@
   :bind (:map lsp-mode-map
               ("s-<f7>" . lsp-find-references)
               ("M-g d" . lsp-find-definition)
-              ("M-/" . completion-at-point)))
+              ("M-/" . completion-at-point))
+  :config
+  ;; Some performance tuning as recommended here:  https://emacs-lsp.github.io/lsp-mode/page/performance
+  ;; Increase gc threshold to 100MB (default is 800kB)
+  (setq gc-cons-threshold (* 100 1000 1000))
+  ;; Increase buffer size for reading subprocess output to 1MB (default is 4kB)
+  (setq read-process-output-max (* 1024 1024))
+  )
 (use-package lsp-ui
   :ensure t
   :bind (:map lsp-ui-mode-map
