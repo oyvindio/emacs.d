f7d576fe1bc400c90786441a343db420d5ee8b60
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-10-05 10:12:20 +0200

    Put defadvice and functions in use-package form

--- auto-loaded-lisp/my-magit.el
+++ auto-loaded-lisp/my-magit.el
@@ -1,18 +1,19 @@
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-
-(defun magit-quit-session ()
-  "Restores the previous window configuration and kills the magit buffer"
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
-
 (use-package magit
   :ensure t
   :config
   (setq magit-diff-refine-hunk 'all)
+
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+
+  (defun magit-quit-session ()
+    "Restores the previous window configuration and kills the magit buffer"
+    (interactive)
+    (kill-buffer)
+    (jump-to-register :magit-fullscreen))
+
   :commands magit-status
   :bind
   ("C-x g" . magit-status)
@@ -21,13 +22,14 @@
   (:map magit-status-mode-map
         ("q" . magit-quit-session)))
 
-(defun set-git-commit-fill-column ()
-  (setq fill-column 72))
-
 (use-package git-commit
   :ensure t
   :config
   (setq git-commit-summary-max-length 50)
+
+  (defun set-git-commit-fill-column ()
+    (setq fill-column 72))
+
   (add-hook 'git-commit-mode-hook 'set-git-commit-fill-column))
 
 (provide 'my-magit)
