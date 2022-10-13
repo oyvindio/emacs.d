2673b15b8a9f50a8116f8867aa5d90e6a747eab7
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-10-13 16:39:31 +0200

    Remove nonfunctional org autodone

--- auto-loaded-lisp/my-org.el
+++ auto-loaded-lisp/my-org.el
@@ -1,8 +1,3 @@
-(defun my-org-autodone (n-done n-not-done)
-  "Switch entry to DONE when all subentries are done, to TODO otherwise."
-  (let (org-log-done org-log-states)   ; turn off logging
-    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
-
 (use-package org
   :ensure t
   :bind (:map org-mode-map
@@ -14,7 +9,6 @@
   (setq org-clock-rounding-minutes 5)
   (setq org-clock-clocked-in-display nil)
   (setq org-clock-persist t)
-  (add-hook 'org-after-todo-statistics-hook 'my-org-autodone)
   (org-clock-persistence-insinuate)
   (add-hook 'org-mode-hook 'visual-line-mode))
 
