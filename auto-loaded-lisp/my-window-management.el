bd5bd3832a0158d0d93a5ce5f7b9ec069f97f937
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-12-06 13:47:50 +0100

    Handle flycheck errors buffer as side/bottom window

---
+++ auto-loaded-lisp/my-window-management.el
@@ -0,0 +1,11 @@
+(setq switch-to-buffer-obey-display-actions t)
+
+(add-to-list 'display-buffer-alist
+   '("\\*Flycheck errors\\*"
+     (display-buffer-in-side-window
+      (window-height . 0.25)
+      (side . bottom))))
+
+(setq display-buffer-alist nil)
+
+(provide 'my-window-management)
