b3163c2947920198fbd91f889309f079d2c4fe10
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2022-12-06 13:47:09 +0100

    Unbind C-z

--- auto-loaded-lisp/bindings.el
+++ auto-loaded-lisp/bindings.el
@@ -115,4 +115,7 @@
     (let ((case-fold-search isearch-case-fold-search))
       (occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))))
 
+;; unbind suspend-frame from this to avoid accidental minimize window
+(global-unset-key (kbd "C-z"))
+
 (provide 'bindings)
