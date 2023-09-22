dcadf331236a27476d072b3b4bbfc8a40318ee0f
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2023-09-22 08:55:47 +0200

    Bind eglot organize imports

--- auto-loaded-lisp/my-eglot.el
+++ auto-loaded-lisp/my-eglot.el
@@ -5,7 +5,8 @@
               ("s-l d" . eldoc)
               ("s-l e" . flymake-show-buffer-diagnostics)
               ("s-l r" . eglot-rename)
-              ("s-l =" . eglot-format-buffer)
+              ("s-l =" . eglot-format)
+              ("s-l i" . eglot-code-action-organize-imports)
               )
   )
 
