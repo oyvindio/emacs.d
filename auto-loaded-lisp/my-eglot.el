4bb38b862d393f778cc294c4745b9df02d14f91d
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2023-10-13 13:02:31 +0200

    More eglot binds

--- auto-loaded-lisp/my-eglot.el
+++ auto-loaded-lisp/my-eglot.el
@@ -4,9 +4,12 @@
               ("s-l" . nil)
               ("s-l d" . eldoc)
               ("s-l e" . flymake-show-buffer-diagnostics)
-              ("s-l r" . eglot-rename)
+              ("s-l r" . eglot-rename) ; Use M-? (`xref-find-references`), then r to query replace
               ("s-l =" . eglot-format)
               ("s-l i" . eglot-code-action-organize-imports)
+              ("s-l g i" . eglot-find-implementation)
+              ;; ("s-l g d" . eglot-find-declaration) ; use M-. (`xref-find-definitions`)
+              ("s-l g t" . eglot-find-typeDefinition)
               )
   )
 
