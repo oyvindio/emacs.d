888e63344cf743a854ab0c3d213f10f595129539
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2024-09-24 09:07:17 +0200

    Eglot bindincs

--- auto-loaded-lisp/my-eglot.el
+++ auto-loaded-lisp/my-eglot.el
@@ -4,12 +4,14 @@
               ("s-l" . nil)
               ("s-l d" . eldoc)
               ("s-l e" . flymake-show-buffer-diagnostics)
-              ("s-l r" . eglot-rename) ; Use M-? (`xref-find-references`), then r to query replace
+              ;; ("s-l r" . eglot-rename) ; Use M-? (`xref-find-references`), then r to query replace
               ("s-l =" . eglot-format)
               ("s-l i" . eglot-code-action-organize-imports)
               ("s-l g i" . eglot-find-implementation)
               ;; ("s-l g d" . eglot-find-declaration) ; use M-. (`xref-find-definitions`)
               ("s-l g t" . eglot-find-typeDefinition)
+              ("C-x M-." . xref-find-definitions-other-window)
+              ("C-x M-?" . xref-find-references-and-replace)
               )
   )
 
