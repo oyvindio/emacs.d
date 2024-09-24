3e73a4bafc1cd2947755b963830681b20010cfe1
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2024-09-24 09:05:30 +0200

    Add binding for projectile

--- auto-loaded-lisp/my-projectile.el
+++ auto-loaded-lisp/my-projectile.el
@@ -1,5 +1,6 @@
 (use-package projectile
   :ensure t
+  :bind (:map projectile-mode-map ("C-c p" . projectile-command-map))
   :config
   (projectile-global-mode t)
   ; default projectile-mode-line is very slow and is executed on {next,previous}-line which makes navigation very slow
